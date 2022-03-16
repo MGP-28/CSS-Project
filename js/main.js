049018bc3b5421b05acb07e77db1c7cb4a757415
MGP-28 <joaosilvapereira96@hotmail.com>
2022-03-16 01:59:30 +0000

    audio easter egg finished; easter egg relocated; code cleanup

---
+++ js/main.js
@@ -0,0 +1,17 @@
+isActive = false;
+function playAudio(){
+	document.getElementsByTagName("audio")[0].loop = true;
+	var audio = document.getElementsByTagName("audio")[0];
+	if(!isActive){
+		isActive = true;
+		audio.play();
+	}
+	else{
+		isActive = false;
+		audio.pause();
+	}
+}
+function resetAudio(){
+	var audio = document.getElementsByTagName("audio")[0];
+	audio.currentTime = 0;
+}
