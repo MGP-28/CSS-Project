9f896285a23dd3497e1518fd835ad2339b225b19
MGP-28 <joaosilvapereira96@hotmail.com>
2022-03-16 22:49:23 +0000

    form layout fixed; easter egg audio changed

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ isActive = false;
 function playAudio(){
 	document.getElementsByTagName("audio")[0].loop = true;
 	var audio = document.getElementsByTagName("audio")[0];
+	audio.currentTime = 0;
 	if(!isActive){
 		isActive = true;
 		audio.play();
